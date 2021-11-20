336eee1235318c633fa5b1fb3b5bbdd6d4799e0b
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-11-20 13:49:39 -0500

    added test cases for whiskey

--- src/main/java/models/Whiskey.java
+++ src/main/java/models/Whiskey.java
@@ -9,4 +9,59 @@ public class Whiskey {
     private int qty;
     private float price;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getProof() {
+        return proof;
+    }
+
+    public void setProof(int proof) {
+        this.proof = proof;
+    }
+
+    public int getBottleSize() {
+        return bottleSize;
+    }
+
+    public void setBottleSize(int bottleSize) {
+        this.bottleSize = bottleSize;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
--- src/test/java/models/WhiskeyTest.java
+++ src/test/java/models/WhiskeyTest.java
@@ -1,7 +1,86 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WhiskeyTest {
+    @Test
+    public void setIDTest() {
+        // given (1)
+        int expected = 1;
+        // when (2)
+        Whiskey testWhiskey = new Whiskey();
+        testWhiskey.setId(expected);
+        // then (3)
+        Assertions.assertEquals(expected, testWhiskey.getId());
+    }
+
+    @Test
+    public void setNameTest() {
+        // given (1)
+        String expected = "Rye";
+        // when (2)
+        Whiskey testWhiskey = new Whiskey();
+        testWhiskey.setName(expected);
+        // then (3)
+        Assertions.assertEquals(expected, testWhiskey.getName());
+    }
+
+    @Test
+    public void setBrandTest() {
+        // given (1)
+        String expected = "Maker's Mark";
+        // when (2)
+        Whiskey testWhiskey = new Whiskey();
+        testWhiskey.setBrand(expected);
+        // then (3)
+        Assertions.assertEquals(expected, testWhiskey.getBrand());
+    }
+
+    @Test
+    public void setProofTest() {
+        // given (1)
+        int expected = 100;
+        // when (2)
+        Whiskey testWhiskey = new Whiskey();
+        testWhiskey.setProof(expected);
+        // then (3)
+        Assertions.assertEquals(expected, testWhiskey.getProof());
+    }
+
+    @Test
+    public void setBottleSizeTest() {
+        // given (1)
+        int expected = 750;
+        // when (2)
+        Whiskey testWhiskey = new Whiskey();
+        testWhiskey.setBottleSize(expected);
+        // then (3)
+        Assertions.assertEquals(expected, testWhiskey.getBottleSize());
+    }
+
+    @Test
+    public void setQtyTest() {
+        // given (1)
+        int expected = 10;
+        // when (2)
+        Whiskey testWhiskey = new Whiskey();
+        testWhiskey.setQty(expected);
+        // then (3)
+        Assertions.assertEquals(expected, testWhiskey.getQty());
+    }
+
+    @Test
+    public void setPriceTest() {
+        // given (1)
+        float expected = 40.50f;
+        // when (2)
+        Whiskey testWhiskey = new Whiskey();
+        testWhiskey.setPrice(expected);
+        // then (3)
+        Assertions.assertEquals(expected, testWhiskey.getPrice());
+    }
 
 }
