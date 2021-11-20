d5e81123c941550b4e215443c8ffdcfa9a36b33e
cay <cay@zipcodes-MBP-9.fios-router.home>
2021-11-20 13:25:01 -0500

    added whiskey properties

--- src/main/java/models/Whiskey.java
+++ src/main/java/models/Whiskey.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Whiskey {
+    private int id;
+    private String name;
+    private String brand;
+    private int proof;
+    private int bottleSize;
+    private int qty;
+    private float price;
+
 }
